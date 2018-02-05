74d70a90c23e31bbc8834288996f8fe2f8d87b50
Michaël Vanbrabandt <info@mivaweb.be>
2018-02-05 21:39:47 +0100

    Commit source files of the MemberLogin solution

---
+++ src/MemberLogin/Controllers/MemberLoginApiController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Security;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+namespace MemberLogin.Controllers
+{
+    /// <summary>
+    /// Implement the MemberLoginApiController
+    /// </summary>
+    [PluginController("MemberLogin")]
+    public class MemberLoginApiController : UmbracoAuthorizedApiController
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        public MemberLoginApiController() { }
+
+        /// <summary>
+        /// Do Login
+        /// </summary>
+        [HttpPost]
+        public void DoLogin([FromBody]Guid member)
+        {
+            // Get the member using the Guid
+            var _member = Services.MemberService.GetByKey(member);
+
+            // Only do the login when we have a member
+            if (_member != null)
+                FormsAuthentication.SetAuthCookie(_member.Username, false);
+        }
+    }
+}
