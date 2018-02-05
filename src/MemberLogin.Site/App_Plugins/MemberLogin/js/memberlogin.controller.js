335c59eb8d70d612650fba3c09450dec757c696e
Michaël Vanbrabandt <info@mivaweb.be>
2018-02-05 22:38:18 +0100

    Create Grunt file to copy src files to Site project

---
+++ src/MemberLogin.Site/App_Plugins/MemberLogin/js/memberlogin.controller.js
@@ -0,0 +1,62 @@
+﻿(function () {
+
+    'use strict';
+
+    function memberLoginController(
+        $scope,
+        $http,
+        editorState,
+        contentResource) {
+
+        var vm = this;
+
+        vm.isNew = false;
+
+        // Check if you are creating a new member
+        vm.isNew = editorState.current.id <= 0;
+
+        // Define the login as member function
+        vm.loginAsMember = login;
+ 
+
+        //////////////////////////////////
+
+
+        function login() {
+
+            // ### Setup cookie
+            var url = 'backoffice/memberlogin/memberloginapi/dologin';
+
+            // Get the current member id using the editorState
+            var _memberId = editorState.current.id;
+
+            // Do Login
+            $http.post(
+                url,
+                _memberId).then(
+                function (response) {
+
+                    // ### Redirect
+                    // Get the redirect page from config
+                    var urlPageRedirect = $scope.model.config.memberRedirectPage;
+
+                    // Check if page is set in the config
+                    if (urlPageRedirect) {
+                        contentResource.getNiceUrl(urlPageRedirect).then(function (data) {
+                            window.open(data, '_blank') // Get the first url
+                        });
+                    } else {
+                        // Open the root page
+                        window.open('/', '_blank');
+                    }
+
+                },
+                function (error) { }
+            );
+        }
+    }
+
+    angular.module('umbraco')
+        .controller('MemberLoginController', memberLoginController);
+
+})();
---
+++ src/MemberLogin/gruntfile.js
@@ -0,0 +1,45 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+
+        // Clean the Site project /App_Plugins/MemberLogin folder
+        clean: {
+            folder: ['../MemberLogin.Site/App_Plugins/MemberLogin'],
+            options: { force: true }
+        },
+
+        // Create /App_Plugins/MemberLogin folder
+        mkdir: {
+            all: {
+                options: {
+                    create: ['../MemberLogin.Site/App_Plugins/MemberLogin']
+                }
+            }
+        },
+
+        // Copy files to the /App_Plugins/MemberLogin folder
+        copy: {
+            files: {
+                cwd: 'Web/UI/App_plugins/MemberLogin',
+                src: '**/*',
+                dest: '../MemberLogin.Site/App_Plugins/MemberLogin',
+                expand: true
+            }
+        },
+
+        // Watch when files are updated => copy to the Site project /App_Plugins/MemberLogin folder
+        watch: {
+            memberlogin: {
+                files: ['Web/UI/App_plugins/MemberLogin/**/*'],
+                tasks: ['copy']
+            }
+        }
+
+    });
+
+
+    // Load NPM tasks
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-mkdir');
+};
