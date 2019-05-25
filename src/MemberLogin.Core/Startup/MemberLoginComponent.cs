17aba9f5af00e623c885d50349ccf37ed5e30ab1
Michaël Vanbrabandt <info@mivaweb.be>
2019-05-25 15:30:36 +0200

    Setup new projects using the .net framework 4.7.2 / add custom menu item on the members tree

---
+++ src/MemberLogin.Core/Startup/MemberLoginComponent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Composing;
+using Umbraco.Web.Trees;
+
+namespace MemberLogin.Core.Startup
+{
+    public class MemberLoginComponent : IComponent
+    {
+        public void Initialize()
+        {
+            TreeControllerBase.MenuRendering += MemberLogin_MenuRendering;
+        }
+
+        private void MemberLogin_MenuRendering(TreeControllerBase sender, MenuRenderingEventArgs e)
+        {
+            // If we are not on the members tree, just return
+            if (sender.TreeAlias != "member") return;
+
+            var menuItem = new Umbraco.Web.Models.Trees.MenuItem("memberLogin", "Impersonate Member");
+            menuItem.Icon = "client";
+            menuItem.AdditionalData.Add("memberId", e.NodeId);
+            menuItem.AdditionalData.Add("actionView", "/App_Plugins/MemberLogin/views/memberlogin.html");
+            menuItem.SeparatorBefore = false;
+
+            e.Menu.Items.Insert(e.Menu.Items.Count, menuItem);
+        }
+
+        public void Terminate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/MemberLogin.Core/Startup/MemberLoginComposer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace MemberLogin.Core.Startup
+{
+    public class MemberLoginComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Components().Append<MemberLoginComponent>();
+        }
+    }
+}
