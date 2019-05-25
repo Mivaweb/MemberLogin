4d0cb78d2d03ce9c2e7bc66a65a2669854e7638d
Michaël Vanbrabandt <info@mivaweb.be>
2019-05-25 22:56:58 +0200

    Commit changes on making memberlogin compatible on umbraco v8

--- src/MemberLogin.Core/Web/UI/App_Plugins/MemberLogin/js/memberlogin.controller.js
+++ src/MemberLogin.Core/Web/UI/App_Plugins/MemberLogin/js/memberlogin.controller.js
@@ -7,19 +7,19 @@
             $http,
             editorState,
             navigationService,
-            contentResource
+            contentResource,
+            editorService
         ) {
 
         var vm = this;
 
         vm.member = editorState.current.name;
         vm.node = null;
-        vm.contentId = 0;
 
         vm.init = init;
         vm.close = close;
-        vm.reset = reset;
         vm.login = login;
+        vm.pickRedirectPage = pickRedirectPage;
 
 
 
@@ -27,81 +27,32 @@
 
 
 
+        // Open editorService contentpicker
+        function pickRedirectPage() {
+            navigationService.allowHideDialog(false);
+            editorService.contentPicker({
+                submit: function (model) {
+                    // Get the first selected content node
+                    vm.node = model.selection[0];
+
+                    editorService.close();
+                    navigationService.allowHideDialog(true);
+                },
+                close: function () {
+                    editorService.close();
+                    navigationService.allowHideDialog(true);
+                }
+            });
+        }
+
         // ## Init dialog
         function init() {
 
-            // Set the modal to be passed through the contentPicker
-            $scope.model = {
-                view: 'itempicker',
-                title: 'Select content',
-                size: 'small',
-                section: 'content',
-                treeAlias: 'content',
-                multiPicker: false,
-                show: true
-                //hideSubmitButton: true,
-                //submit: function (model) {
-
-                //    // Get the first selected content node
-                //    var data = model.selection[0];
-
-                //    setContent(data);
-
-                //}
-            }
-
-            // Set view
-            vm.view = $scope.model.view;
-
-            setView();
         }
 
         // ## Close navigation
         function close() {
             navigationService.hideNavigation();
-        }
-
-        // ## Reset
-        function reset() {
-            setContent();
-        }
-
-        // ## Set view
-        function setView() {
-
-            if (vm.view) {
-                
-                if (vm.view.indexOf(".html") === -1) {
-                    var viewAlias = vm.view.toLowerCase();
-                    vm.view = "views/common/overlays/" + viewAlias + "/" + viewAlias + ".html";
-                }
-
-            }
-
-        }
-
-        // ## Set Content in UI
-        function setContent(node) {
-
-            // Reset
-            if (!node) {
-                vm.contentId = 0;
-                vm.node = null;
-                return;
-            }
-
-            // Set the ContentId
-            vm.contentId = node.id;
-
-            // Get node data
-            contentResource.getById(vm.contentId).then(function (data) {
-
-                // Update the UI
-                vm.node = data;
-                vm.node.status = !vm.node.published ? 'This item is not published' : '';
-
-            });
-
         }
 
         // ## Login as the selected member
@@ -117,12 +68,12 @@
             $http.post(
                 url,
                 _memberId).then(
-                function (response) {
+                function () {
 
                     // ### Redirect
                     // Check if page is set in the config
-                    if (vm.contentId) {
-                        contentResource.getNiceUrl(vm.contentId).then(function (data) {
+                    if (vm.node && vm.node.id) {
+                        contentResource.getNiceUrl(vm.node.id).then(function (data) {
                             window.open(data, '_blank')
                         });
                     } else {
--- src/MemberLogin.SiteV8/App_Data/Models/models.generated.cs
+++ src/MemberLogin.SiteV8/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "fa8850d126754877")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 namespace Umbraco.Web.PublishedModels
 {
