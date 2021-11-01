c89c833094d9eb99f3fb853ef04a0759911a4a47
Aaron Sadler <aaron@aaronsadler.uk>
2021-11-01 18:04:36 +0000

    Fixed backoffice menu

--- src/MemberLoginV9.Core/TreeNotificationHandlers/MemberLoginTreeNotificationHandler.cs
+++ src/MemberLoginV9.Core/TreeNotificationHandlers/MemberLoginTreeNotificationHandler.cs
@@ -1,33 +1,26 @@
-﻿using System.Linq;
-using Umbraco.Cms.Core.Events;
+﻿using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Notifications;
-using Umbraco.Cms.Core.Security;
-using Umbraco.Extensions;
 
 namespace MemberLoginV9.Core.TreeNotificationHandlers
 {
     public class MemberLoginTreeNotificationHandler : INotificationHandler<MenuRenderingNotification>
     {
-        private readonly IBackOfficeSecurityAccessor _backOfficeSecurityAccessor;
-
-        public MemberLoginTreeNotificationHandler(IBackOfficeSecurityAccessor backOfficeSecurityAccessor)
+        public MemberLoginTreeNotificationHandler()
         {
-            _backOfficeSecurityAccessor = backOfficeSecurityAccessor;
         }
 
         public void Handle(MenuRenderingNotification notification)
         {
-            if (notification.TreeAlias.Equals("member") &&
-                _backOfficeSecurityAccessor.BackOfficeSecurity.CurrentUser.Groups.Any(x =>
-                    x.Alias.InvariantEquals("admin")))
-            {
-                var menuItem = new Umbraco.Cms.Core.Models.Trees.MenuItem("memberLogin", "Impersonate Member");
-                menuItem.AdditionalData.Add("memberId", notification.NodeId);
-                menuItem.AdditionalData.Add("actionView", "/App_Plugins/MemberLogin/views/memberlogin.html");
-                menuItem.Icon = "client";
-                menuItem.SeparatorBefore = false;
-                notification.Menu.Items.Insert(notification.Menu.Items.Count, menuItem);
-            }
+            // If we are not on the members tree, just return
+            if (notification.TreeAlias != "member") return;
+
+            var menuItem = new Umbraco.Cms.Core.Models.Trees.MenuItem("memberLogin", "Impersonate Member");
+            menuItem.AdditionalData.Add("memberId", notification.NodeId);
+            menuItem.AdditionalData.Add("actionView", "/App_Plugins/MemberLoginV9/views/memberlogin.html");
+            menuItem.Icon = "client";
+            menuItem.SeparatorBefore = false;
+            notification.Menu.Items.Insert(notification.Menu.Items.Count, menuItem);
+
         }
     }
 }
